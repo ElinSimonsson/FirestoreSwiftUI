f03decce6081a2294207c8e60520ee368acf7c60
Elin <elin.simonsson@outlook.com>
2023-01-03 13:47:40 +0100

    Initial Commit

---
+++ FirebaseStorage/FirebaseStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseStorageApp.swift
+//  FirebaseStorage
+//
+//  Created by Elin Simonsson on 2023-01-03.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

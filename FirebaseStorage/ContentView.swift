1feb8b8a0dda8feb34405d33a035771419d655cc
Elin <elin.simonsson@outlook.com>
2023-01-09 14:09:52 +0100

    added progressBarView and the possibility to sign up, sign in and sign out

--- FirebaseStorage/ContentView.swift
+++ FirebaseStorage/ContentView.swift
@@ -21,8 +21,7 @@ struct ContentView: View {
         if !signedIn {
             SigningInView(signedIn: $signedIn)
         } else   {
-            ShoppinListView()
-           // SettingsView()
+            ShoppingListView()
         }
     }
 }
@@ -31,50 +30,46 @@ struct SigningInView: View {
     @Binding var signedIn : Bool
     
     var body: some View {
-        Text("Signing in...")
-            .onAppear() {
-                let user = Auth.auth().currentUser
-                if user == nil {
-                    Auth.auth().signInAnonymously { authResult, error in
-                        if let error = error {
-                            print("error signing in \(error)")
-                            
-                        } else {
-                            print("Signed in \(Auth.auth().currentUser?.uid)")
-                            signedIn = true
+        
+        if !signedIn {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .orange))
+                .scaleEffect(3)
+                .onAppear() {
+                    let user = Auth.auth().currentUser
+                    if user == nil {
+                        Auth.auth().signInAnonymously { authResult, error in
+                            if let error = error {
+                                print("error signing in \(error)")
+                                
+                            } else {
+                                print("Signed in \(Auth.auth().currentUser!.uid)")
+                                signedIn = true
+                            }
                         }
+                    } else {
+                        signedIn = true
+                        print("redan inloggad")
                     }
-                } else {
-                    signedIn = true
-                    print("redan inloggad")
-            }
-                
+                }
             }
     }
 }
 
-struct ShoppinListView: View {
+struct ShoppingListView: View {
     let db = Firestore.firestore()
     @State private var newItemName = ""
     @State var item = [Item]()
     @ObservedObject var items = Items()
     @State var isAddingItem = false
-    @State var emptyList = true
-    //@ObservedObject var test = Items()
+    
     
     var body: some View {
-        
         NavigationView {
             VStack {
-                if emptyList {
-                    Text("Tap to add a new item")
-                        .font(.headline)
-                        .fontWeight(.regular)
-                }
                 List {
-                    
                     ForEach(items.items) { item in
-                        ItemRowView(item: item, emptyList: $emptyList)
+                        ItemRowView(item: item)
                     }.onDelete() { indexSet in
                         items.deleteItem(indexSet: indexSet)
                     }
@@ -87,23 +82,17 @@ struct ShoppinListView: View {
                     Image(systemName: "gearshape")
                 })
                 .onTapGesture {
-                    self.emptyList = false
                     self.isAddingItem = true
                 }
-                .onReceive(items.objectWillChange) { // tillfällig lösning
-                               if items.test {
-                                   print("items.test är sant")
-                               } else {
-                                   print("items.test är falskt")
-                               }
-                           }
+                .onReceive(items.objectWillChange) {
+                    if items.test {
+                        isAddingItem = false
+                    } else {
+                        isAddingItem = true
+                    }
+                }
             }.onAppear() {
                 items.listenToFirestore()
-                if items.test{
-                    print("sant")
-                } else {
-                    print("falsk")
-                }
             }
         }
     }
@@ -113,59 +102,106 @@ struct SettingsView : View {
     let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
     @State var email = ""
     @State var password = ""
-    
+    @State var showCreateAccountView = false
+    @Environment(\.presentationMode) var presentationMode
+    @State var isAnonymous = false
+    @State var showText = true
+    @State var showContentView = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            WelcomeText()
-            Spacer()
-            UserImage()
-            TextField("Email", text: $email)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-            SecureField("Password", text: $password)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-            //Spacer()
-            Text("Create an new account here")
-                .foregroundColor(.blue)
-                .onTapGesture {
-                    print("the user want to create an new account")
+        if showText {
+            Text("")
+                .onAppear(){
+                    guard let user = Auth.auth().currentUser else {return}
+                    if user.isAnonymous {
+                        isAnonymous = true
+                        showText = false
+                    } else {
+                        isAnonymous = false
+                        showText = false
+                    }
                 }
-            Spacer()
-            Button(action: {
-                print("Button tapped! \(email), \(password)")
-                createAccount()
-            }) {
-                LoginButtonContent()
-            }
-            Spacer()
         }
-        .padding()
+        if isAnonymous {
+            VStack {
+                Spacer()
+                WelcomeText()
+                Spacer()
+                UserImage()
+                TextField("Email", text: $email)
+                    .padding()
+                    .background(lightGreyColor)
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(lightGreyColor)
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                Button(action: {
+                    showCreateAccountView = true
+                }) {
+                    Text("Create an new account here!")
+                }
+                .fullScreenCover(isPresented: $showCreateAccountView, content: CreateAccountView.init)
+                
+                Spacer()
+                Button(action: {
+                    print("Button tapped! \(email), \(password)")
+                    signIn()
+                }) {
+                    LoginButtonContent(signText: "LOGIN")
+                }
+                Spacer()
+            }
+            .padding()
+            
+        } else {
+            Text("the user is NOT anonymous")
+            UserImage()
+                .padding(.top, 20)
+            Button(action: {
+                signOut()
+            }) {
+                LoginButtonContent(signText: "SIGN OUT")
+            }
+            .fullScreenCover(isPresented: $showContentView, content: ContentView.init) // hur backar man direkt till contentView utan att skapa ny instans av contentView?
+        }
     }
     
-    func createAccount () {
+    func signIn () {
         let email = $email.wrappedValue
         let password = $password.wrappedValue
-        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        
+        
+        Auth.auth().signIn(withEmail: email, password: password){ authResult, error in
             if let error = error {
                 print("error signing in \(error)")
             } else {
-                print("signed in \(authResult?.user.uid)")
+                print("signed in \(authResult!.user.uid)")
+                presentationMode.wrappedValue.dismiss()
             }
+        }
+        
+    }
+    
+    func signOut () {
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            showContentView = true
+        } catch let signOutError as NSError {
+            print("Error signing out: ‰@", signOutError)
         }
     }
     
 }
 
 struct LoginButtonContent : View {
+    var signText : String
     var body: some View {
-        Text("LOGIN")
+        Text(signText)
             .font(.headline)
             .foregroundColor(.white)
             .padding()
@@ -182,7 +218,6 @@ struct WelcomeText : View {
             .fontWeight(.semibold)
             .padding(.bottom, 20)
     }
-    
 }
 
 struct UserImage : View {
@@ -194,7 +229,89 @@ struct UserImage : View {
             .clipped()
             .cornerRadius(150)
             .padding(.bottom, 75)
+        
+    }
+}
 
+struct CreateAccountText : View {
+    var body : some View {
+        Text("Creating an new account")
+            .font(.title)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct CreateAccountView : View {
+    let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+    @Environment(\.presentationMode) var presentationMode
+    @State var showContentView = false
+    @State var email = ""
+    @State var password = ""
+    @State var repeatPassword = ""
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("< Back")
+                    .padding(.leading, 10)
+                    .font(.title2)
+            }
+            Spacer()
+        }
+        VStack {
+            Spacer()
+            CreateAccountText()
+            Spacer()
+            UserImage()
+            TextField("Email", text: $email)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Repeat password", text: $repeatPassword)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            
+            Button(action: {
+                //presentationMode.wrappedValue.dismiss()
+                createAccount()
+            }) {
+                LoginButtonContent(signText: "Sign up")
+            }
+            .fullScreenCover(isPresented: $showContentView, content: ContentView.init) // hur backar man direkt till contentView utan att skapa ny instans av contentView?
+            .padding(.top, 20)
+        }
+        .padding()
+    }
+    
+    func createAccount() {
+        let email = $email.wrappedValue
+        let password = $password.wrappedValue
+        let repeatPassword = $repeatPassword.wrappedValue
+        
+        if password == repeatPassword {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    print("error signing in \(error)")
+                } else {
+                    showContentView = true
+                    print("signed in \(authResult?.user.uid)")
+                    
+                }
+            }
+        } else {
+            print("lösenord stämmer inte")
+        }
     }
 }
 
@@ -222,7 +339,6 @@ struct ItemRowView: View {
     
     let item : Item
     @StateObject var items = Items()
-    @Binding var emptyList : Bool
     
     var body: some View {
         HStack {
@@ -236,7 +352,7 @@ struct ItemRowView: View {
                 }
         }.onAppear() {
             print("listan är här")
-            emptyList = false
+            
         }
     }
 }
